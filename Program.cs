50af33346e2fe73ab0c69eb05156251cb0b74ca2
Mikolaj <sunvision90@gmail.com>
2024-03-19 23:10:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Podaj 1 liczbę:");
+
+                var number1 = GetInput();
+
+
+                Console.WriteLine("Jaką operacje chcesz wykonać? Możliwe operacje to:  '+', '-', '/', '*'.");
+                var action = Console.ReadLine();
+
+                Console.WriteLine("Podaj 2 liczbę:");
+                var number2 = GetInput();
+
+                var result = Calculate(number1, number2, action);
+
+
+                Console.WriteLine("Wynik Twojego działania to: " + result);
+
+            }
+            catch (Exception ex)
+            {
+               //logowanie do pliku
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private static int GetInput()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int input))
+
+                throw new Exception("Podana wartość jest nieprawidłowa");
+
+            return input;
+
+        }
+
+        private static int Calculate(int number1, int number2, string action)
+        {
+            switch (action)
+            {
+                case "+":
+                    return number1 + number2;
+
+                case "-":
+                    return number1 - number2;
+
+                case "/":
+                    return number1 / number2;
+
+                case "*":
+                    return number1 * number2;
+
+                default:
+                    throw new Exception("Wybrałeś złą operację!");
+
+            }
+        }
+    }
+}
+
